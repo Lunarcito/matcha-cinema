77679d8185996a5043795ed4a13d7b98f1283c7a
Lunarcito <calle.carolina@gmail.com>
2025-02-25 22:23:24 +0100

    Set up Routes in App

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,14 @@
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Home from "./components/Home";
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-      </header>
-    </div>
+    <Router>
+      <Routes>
+        <Route path="/" element={<Home/>}/>
+      </Routes>
+    </Router>
   );
 }
 
