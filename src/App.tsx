18be4055d9bad2ddf5991919994a4bcc7fff1d2f
Home <calle.carolina@gmail.com>
2025-03-13 23:39:32 +0100

    feat: add Layout wrapper to Home route

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,5 @@
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import Layout from './components/Layout';
 import Home from './pages/Homepage/HomePage';
 import Login from './pages/LoginPage/LoginPage';
 import SignUp from './pages/SignupPage/SignupPage';
@@ -8,7 +9,7 @@ function App() {
   return (
     <Router>
       <Routes>
-        <Route path="/" element={<Home />} />
+        <Route path="/" element={<Layout><Home /></Layout>} />
         <Route path="/login" element={<Login />} />
         <Route path="/signup" element={<SignUp />} />
       </Routes>
