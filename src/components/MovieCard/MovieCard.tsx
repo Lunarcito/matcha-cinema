950c2aaed214fcd8dc1b7e61573e598d6e81593d
Home <calle.carolina@gmail.com>
2025-03-16 22:24:03 +0100

    chore: organize MovieCard component

--- src/components/MovieCard/MovieCard.tsx
+++ src/components/MovieCard/MovieCard.tsx
@@ -12,8 +12,13 @@ interface MovieCardProps {
 
 const MovieCard: React.FC<MovieCardProps> = ({ movie }) => {
   return (
-    <Card  sx={{ width: 250, height: 250}} >
-      <CardMedia component="img" height="140" image={movie.imageUrl} alt={movie.title} />
+    <Card sx={{ width: 250, height: 250 }}>
+      <CardMedia
+        component="img"
+        height="140"
+        image={movie.imageUrl}
+        alt={movie.title}
+      />
       <CardContent>
         <Typography variant="h6">{movie.title}</Typography>
         <Typography variant="body2" color="textSecondary">
