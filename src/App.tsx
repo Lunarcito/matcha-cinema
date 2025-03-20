7b115356a569f83dcec585b81ae8bc8617af95f7
Home <calle.carolina@gmail.com>
2025-03-20 22:43:01 +0100

    feat(theme): add custom theme with ThemeProvider

--- src/App.tsx
+++ src/App.tsx
@@ -5,10 +5,25 @@ import Login from './pages/LoginPage/LoginPage';
 import SignUp from './pages/SignupPage/SignupPage';
 import AddMovie from './pages/AddMovies.tsx/AddMovies';
 import './App.css';
+import { createTheme,ThemeProvider } from '@mui/material';
+
+const theme = createTheme({
+  palette: {
+    primary: {
+      main: '#A8C686',
+    },
+    secondary: {
+      main: '#8A9A5B',
+    },
+  },
+  typography: {
+    fontFamily: 'Roboto, sans-serif',
+  },
+});
 
 function App() {
   return (
-
+    <ThemeProvider theme={theme}>
       <Router>
       <Routes>
         <Route path="/" element={<Layout><Home /></Layout>}/>
@@ -17,6 +32,7 @@ function App() {
         <Route path="/addMovies" element={<AddMovie />} />
       </Routes>
     </Router>
+    </ThemeProvider>
     
   );
 }
