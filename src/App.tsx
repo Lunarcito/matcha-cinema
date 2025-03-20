466238bc33c0a7516a833d45e350f63c349090ad
Home <calle.carolina@gmail.com>
2025-03-20 22:41:20 +0100

    feat(routes): add route for AddMovie page

--- src/App.tsx
+++ src/App.tsx
@@ -3,17 +3,21 @@ import Layout from './components/Layout/Layout';
 import Home from './pages/Homepage/HomePage';
 import Login from './pages/LoginPage/LoginPage';
 import SignUp from './pages/SignupPage/SignupPage';
+import AddMovie from './pages/AddMovies.tsx/AddMovies';
 import './App.css';
 
 function App() {
   return (
-    <Router>
+
+      <Router>
       <Routes>
-        <Route path="/" element={<Layout><Home /></Layout>} />
+        <Route path="/" element={<Layout><Home /></Layout>}/>
         <Route path="/login" element={<Login />} />
         <Route path="/signup" element={<SignUp />} />
+        <Route path="/addMovies" element={<AddMovie />} />
       </Routes>
     </Router>
+    
   );
 }
 
